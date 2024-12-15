1aec2f768bd6a6a4d80477efe6703571cb7de483
GrygoryZach <grishazschepilo@gmail.com>
2024-12-15 14:07:28 +0100

    login/logout under routes

--- app.py
+++ app.py
@@ -1,26 +1,55 @@
 from flask import Flask, redirect, render_template
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from data import db_session
 from data.__all_models import User, Deck, Card
+from forms.user import UserLogInForm, UserSignUpForm
+from config import config
 
-app = Flask(__name__)
+template_dir = "templates"
+static_dir = "static"
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+app.config['SECRET_KEY'] = config.SECRET_KEY
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.route('/')
 def index():
-    # TODO Ako korisnik nije ulogovan
-    if True:
+    if not current_user.is_authenticated:
         return render_template('index.html')
-    else:
-        return redirect('/login')
+    return render_template('index.html', message="You are logged in")
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login_page():
-    # TODO Login
-    if True:
-        return render_template('login.html')
-    else:
-        return redirect('/dashboard')
+    login_form = UserLogInForm()
+    if login_form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.username == login_form.username.data).first()
+        if user and user.check_password(login_form.password.data):
+            login_user(user, remember=login_form.remember_me.data)
+            return redirect("/")
+        return render_template(
+            "login.html",
+            login_form=login_form,
+            message="false_mail_or_password"  # Ne postoji nalog sa takvim korisničkim imenom i šifrom
+        )
+    return render_template("login.html", login_form=login_form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
 
 @app.route('/signup')
 def signup_page():
@@ -29,6 +58,7 @@ def signup_page():
         return render_template('signup.html')
     else:
         return redirect('/dashboard')
+
 
 if __name__ == '__main__':
     db_session.global_init("db/library.sqlite")
---
+++ forms/user.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.fields.simple import PasswordField, BooleanField, EmailField
+from wtforms.validators import DataRequired, EqualTo
+
+
+class UserLogInForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign in')
+
+
+class UserSignUpForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    repeat_password = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign up')
