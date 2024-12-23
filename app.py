1bb552cb63f03f8c8debffd23d0114b34d9e18fd
yxzhin <yxzhin@gmail.com>
2024-12-23 21:11:30 +0100

    feat: get deck json by id

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from data import db_session
 from data.__all_models import User, Deck, Card
 from forms.user import UserLogInForm, UserSignUpForm
 from config import config
+from json import dumps as json_dumps
+from datetime import datetime
 
 template_dir = "templates"
 static_dir = "static"
@@ -28,12 +30,18 @@ def index():
 
 
 # TODO: deck to json function
+# //ok i'll do it :)
 
 @app.route('/deck/<int:deck_id>')
 def view_deck(deck_id: int):
     db_sess = db_session.create_session()
     deck = db_sess.query(Deck).filter(Deck.id == deck_id).first()
-    user_created = db_sess.query(User).filter(User.id == deck.user_created_id).first()
+
+    if not deck:
+        return "-1"  # //the legendary error :cold_face: :skull:
+
+    user_created = db_sess.query(User).filter(
+        User.id == deck.user_created_id).first()
     cards = db_sess.query(Card).filter(Card.deck_id == deck.id).all()
     cards_json = [card.to_dict() for card in cards]
     deck_dict = {
@@ -42,7 +50,23 @@ def view_deck(deck_id: int):
         "time_changed": deck.time_changed,
         "cards": cards_json
     }
+
+    # //made by @yxzhin with <3
+    # //#hellokittysupremacy #finelcomeback #yxzhinsave
+
+    if request.args.get("secret") == config.BOT_SECRET:
+
+        # //converting datetime objects because they're not json serializable
+
+        time_changed: datetime = deck_dict["time_changed"]
+        y, mt, d, h, mn, s, ms = time_changed.year, time_changed.month, time_changed.day, time_changed.hour, time_changed.minute, time_changed.second, time_changed.microsecond
+        deck_dict["time_changed"] = fr"{y}-{mt}-{d} {h}-{mn}-{s}-{ms}"
+
+        return json_dumps(deck_dict)
+
     return render_template("view_deck.html", deck=deck_dict)
+
+    # //end of my part
 
 
 @app.route('/deck/<int:deck_id>/edit')
@@ -87,7 +111,8 @@ def login_page():
     login_form = UserLogInForm()
     if login_form.validate_on_submit():
         db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.username == login_form.username.data).first()
+        user = db_sess.query(User).filter(
+            User.username == login_form.username.data).first()
         if not (user and user.check_password(login_form.password.data)):
             return render_template(
                 "login.html",
--- config/config.py
+++ config/config.py
@@ -1,1 +1,4 @@
+
+
 SECRET_KEY = "warkndiulsnivu"
+BOT_SECRET = "U5MSdu3b8LdsZ9qgbltuJ_wQSSX4jc2u"
