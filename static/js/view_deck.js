2b33e29627a4a0bdb0391978703504eba47b1a4e
GrygoryZach <grishazschepilo@gmail.com>
2025-02-08 19:52:35 +0100

    feat: deck view page design

--- static/js/view_deck.js
+++ static/js/view_deck.js
@@ -1,24 +1,23 @@
-const addToLibrary = document.getElementById("add-to-library");
 const addOrRemoveDeckButton = document.getElementById("button-add-remove-deck");
-const buttonText = document.getElementById("add-remove-deck-button-text")
-const feedback = document.getElementById("feedback-add-remove-deck-button");
+const buttonText = document.getElementById("add-remove-deck-button-text");
+const flashcard = document.getElementById("flashcard");
 
 addButtonPreload()
 let currentIndex = 0;
 
 
 // Event listener for card click to flip it
-document.getElementById("flashcard").addEventListener("click", flipCard);
+flashcard.addEventListener("click", flipCard);
 
 
 // Adding or deleting the deck from user's library
-addToLibrary.addEventListener("click", createOrDeleteSavedDeck);
+addOrRemoveDeckButton.addEventListener("click", createOrDeleteSavedDeck);
 
 // Event listener for card flipping
 document.addEventListener("keydown", function(event) {
-  if (event.key === " ") {
-    flipCard();
-  }
+    if (event.key === " ") {
+        flipCard();
+    }
 });
 
 // Event listeners for navigation buttons
@@ -31,13 +30,13 @@ updateCard();
 
 async function addButtonPreload() {
     if (!isMyDeck) {
-        addToLibrary.style.display = "block";
+        addOrRemoveDeckButton.style.display = "block";
 
         const savedDeck = await getSavedDeck(currentUserId, deckId);
         if (!savedDeck.error) {
             buttonText.textContent = savedDeck.data
-                ? "Remove from Library"
-                : "Add to Library";
+            ? "Remove from Library"
+            : "Add to Library";
         }
     }
 }
@@ -45,49 +44,49 @@ async function addButtonPreload() {
 
 // Function for card's content updating
 function updateCard() {
-  const frontSide = document.getElementById("front-side");
-  const backSide = document.getElementById("back-side");
-  const cardNumber = document.getElementById("cardNumber");
+    const frontSide = document.getElementById("front-side");
+    const backSide = document.getElementById("back-side");
+    const cardNumber = document.getElementById("cardNumber");
 
-  frontSide.textContent = cards[currentIndex].front;
-  backSide.textContent = cards[currentIndex].back;
-  cardNumber.textContent = `${currentIndex + 1} / ${cards.length}`;
+    frontSide.textContent = cards[currentIndex].front;
+    backSide.textContent = cards[currentIndex].back;
+    cardNumber.textContent = `${currentIndex + 1} / ${cards.length}`;
 
-  // Disable prev button if at the first card
-  document.getElementById("prevButton").disabled = currentIndex === 0;
-  // Disable next button if at the last card
-  document.getElementById("nextButton").disabled = currentIndex === cards.length - 1;
+    // Disable prev button if at the first card
+    document.getElementById("prevButton").disabled = currentIndex === 0;
+    // Disable next button if at the last card
+    document.getElementById("nextButton").disabled = currentIndex === cards.length - 1;
 }
 
 // Function to flip the card around the X-axis
 function flipCard() {
-  const card = document.querySelector(".card");
-  card.classList.toggle("flipped");
+    const card = document.querySelector(".card");
+    card.classList.toggle("flipped");
 }
 
 // Function to move to the next card
 function nextCard() {
-  if (currentIndex < cards.length - 1) {
-    currentIndex++;
-    const card = document.querySelector(".card");
-    card.classList.remove("flipped");
-    card.style.transition = "none";  // Disable transition for instant change
-    updateCard();
-    setTimeout(function() {card.style.transition = "transform 0.6s";}, 50);
-  }
+    if (currentIndex < cards.length - 1) {
+        currentIndex++;
+        const card = document.querySelector(".card");
+        card.classList.remove("flipped");
+        card.style.transition = "none";  // Disable transition for instant change
+        updateCard();
+        setTimeout(function() {card.style.transition = "transform 0.6s";}, 50);
+    }
 }
 
 
 // Function to move to the previous card
 function prevCard() {
-  if (currentIndex > 0) {
-    currentIndex--;
-    const card = document.querySelector(".card");
-    card.classList.remove("flipped");
-    card.style.transition = "none";  // Disable transition for instant change
-    updateCard();
-    setTimeout(function() {card.style.transition = "transform 0.6s";}, 50);
-  }
+    if (currentIndex > 0) {
+        currentIndex--;
+        const card = document.querySelector(".card");
+        card.classList.remove("flipped");
+        card.style.transition = "none";  // Disable transition for instant change
+        updateCard();
+        setTimeout(function() {card.style.transition = "transform 0.6s";}, 50);
+    }
 }
 
 async function createOrDeleteSavedDeck() {
@@ -113,14 +112,17 @@ async function createOrDeleteSavedDeck() {
                 console.log("SavedDeck successfully added.");
             }
 
-            feedback.innerHTML = `<div class="alert alert-success">${requestResponse.message}</div>`;
             buttonText.textContent = buttonText.textContent === "Add to Library"
-                ? "Remove from Library"
-                : "Add to Library";
+            ? "Remove from Library"
+            : "Add to Library";
+            setTimeout(() => {
+                alert(requestResponse.message);
+            }, 50);
+
         }
         catch (error) {
             console.error("Error:", error);
-            feedback.innerHTML = `<div class="alert alert-danger">An error occurred. Please refresh page and try again.</div>`;
+            alert(requestResponse.message);
         }
     }
 }
@@ -174,9 +176,8 @@ async function postSavedDeck(userId, deckId) {
             },
         });
 
-        if (!response.ok) {
-            throw new Error(`Error: ${response.statusText}`);
-        }
+        if (!response.ok) throw new Error(`Error: ${response.statusText}`);
+
         // Parse and return the response data
         const requestResponse = await response.json();
         return requestResponse;
@@ -191,8 +192,8 @@ async function postSavedDeck(userId, deckId) {
 async function deleteSavedDeck(savedDeckId) {
     try {
         const url = new URL('/api/saved_deck', window.location.origin);
-
         url.searchParams.append('saved_deck_id', savedDeckId);
+
         const response = await fetch(url, {
             method: 'DELETE',
             headers: {
@@ -200,9 +201,7 @@ async function deleteSavedDeck(savedDeckId) {
             },
         });
 
-        if (!response.ok) {
-            throw new Error(`Error: ${response.statusText}`);
-        }
+        if (!response.ok) throw new Error(`Error: ${response.statusText}`);
 
         // Parse and return the response data
         const requestResponse = await response.json();
