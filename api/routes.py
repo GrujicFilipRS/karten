13695de0ce7f688b12e5ca6986cc880b40d31abe
GrygoryZach <grishazschepilo@gmail.com>
2025-02-14 18:39:59 +0100

    feat: Deck API for POST

--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify, request
+from flask_login import current_user
 
 from data import db_session
-from data.__all_models import Deck, SavedDeck
+from data.__all_models import Card, Deck, SavedDeck
 
 api_bp = Blueprint('api', __name__)
 
@@ -125,3 +126,37 @@ def delete_created_deck():
         return jsonify({
             "message": str(e)
         }), 500
+
+
+@api_bp.route('/deck', methods=['POST'])
+def create_deck_api():
+    db_sess = db_session.create_session()
+    try:
+        data = request.json
+        deck_name = data.get("deck_name")
+        deck_description = data.get("description")
+        cards = data.get("cards", [])
+
+        new_deck = Deck(
+            user_created_id=current_user.id,
+            name=deck_name,
+            description=deck_description
+        )
+        db_sess.add(new_deck)
+        db_sess.flush()  # Get the ID before commit
+
+        for position, card in enumerate(cards):
+            new_card = Card(
+                deck_id=new_deck.id,
+                position=position,
+                front=card["front"],
+                back=card["back"]
+            )
+            db_sess.add(new_card)
+        db_sess.commit()
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        db_sess.rollback()
+        return jsonify({"error": f"Unexpected error: {str(e)}"}), 500
+    finally:
+        db_sess.close()
